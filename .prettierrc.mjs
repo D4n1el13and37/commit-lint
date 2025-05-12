53ddfe7d212f9bfe38f2bbdb4e3ae55cfd16e472
Daniil Gorokhov <daniil.gorokhov@abgroup.tech>
2025-05-12 11:14:36 +0300

    feat: create PWA application

---
+++ .prettierrc.mjs
@@ -0,0 +1,11 @@
+const config = {
+  endOfLine: 'lf',
+  printWidth: 80,
+  trailingComma: 'all',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  useTabs: false
+};
+
+export default config;
