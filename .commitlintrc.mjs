b701ab108a3d3754d9a33d17dbd7dd94368b54ec
Daniil Gorokhov <daniil.gorokhov@abgroup.tech>
2025-05-13 12:28:31 +0300

    build: update commitlint

--- .commitlintrc.mjs
+++ .commitlintrc.mjs
@@ -14,20 +14,20 @@
 const configuration = {
   rules: {
     /* HEADER type(scope): subject */
+    /* Тип */
     // Тип не может быть пустым
     "type-empty": [2, "never"],
     // Тип всегда только в нижнем регистре
     "type-case": [2, "always", "lower-case"],
-    
-    // Перечислим все возможные варианты коммитов
+    // Перечислим все возможные типы коммитов
     "type-enum": [
       2,
       "always",
       [
-        "docs", // stay
-        "feat", // stay in
-        "fix", // stay in
-        "refactor", // stay in
+        "docs",
+        "feat",
+        "fix", 
+        "refactor", 
         "build",
         "chore",
         "ci",
@@ -40,16 +40,21 @@ const configuration = {
     // Максимальная длина заголовка 100 символов
     "header-max-length": [2, "always", 100],
 
+    /* Область(scope) */
     // Область всегда только в нижнем регистре
     "scope-case": [2, "always", "lower-case"],
 
+    /* Описание */
     // Описание не может быть пустым
     "subject-empty": [2, "never"],
-
     // Описание не должно заканчиваться '.'
     "subject-full-stop": [2, "never", "."],
+    // Описание не быть короче 5
+    "subject-min-length": [2, "always", 5],
+    "subject-exclamation-mark": [2, 'never'],
 
-    /* BODY */
+
+    /* BODY discuss: нужно ли нам вообще тело для коммитов? */
     // Тело коммита должно начинаться с пустой строки
     "body-leading-blank": [2, "always"],
 
